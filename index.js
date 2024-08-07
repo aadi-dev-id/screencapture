6f4072f63b228d0c22c3899d716a7cd528934d6d
Adarsh Maddheshiya <126190158+aadi-dev-id@users.noreply.github.com>
2024-08-07 13:03:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,106 @@
-import { chromium } from 'playwright';
+const cors = require('cors');
+const mime = require('mime');
+const AWS = require('aws-sdk');
+const express = require('express');
+const bodyParser = require('body-parser');
+const responseTime = require('response-time');
+const puppeteer = require('puppeteer');
+const s3ParseUrl = require('s3-url-parser');
+const gm = require('gm').subClass({ imageMagick: '7+' });
+const { Buffer } = require('buffer');
+const { createCanvas, loadImage } = require('canvas');
+const psd = require('psd');
 
-const url = 'https://chaturbuy.com';
-const date = new Date(Date.now());
+const { fromBuffer } = require('file-type-cjs');
 
-const browser = await chromium.launch();
-const page = await browser.newPage();
+const app = express();
 
-await page.goto(url);
+app.use(cors());
+app.use(responseTime());
+app.set('trust proxy', true);
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(bodyParser.urlencoded({
+    limit: '2mb',
+    extended: false,
+}));
 
-const screenshotPath = `screenshots/${date.toISOString()}.png`;
+const TERMINATE_BROWSER_TIMEOUT = 10 * 60 * 1000;
+let browser = null;
+browserTimer = 0;
 
-await page.screenshot({
-  path: screenshotPath,
-  fullPage: false
-})
+async function initiateBrowser() {
+    browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-web-security', '--disable-features=IsolateOrigins,site-per-process', '--flag-switches-begin', '--ignore-certificate-errors', '--flag-switches-end', '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'] });
+    console.log("Browser running!");
+};
 
-await browser.close();
-console.log("done");
+function terminateBrowser() {
+    clearTimeout(browserTimer);
+    browserTimer = setTimeout(() => {
+        browser.close();
+        browser = null;
+    }, TERMINATE_BROWSER_TIMEOUT);
+}
+
+
+const captureScreenshot = async (data) => {
+    if (!browser) await initiateBrowser();
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+    await page.setViewport({ width: 1080, height: 720 });
+    if (isValidURL(data)) await page.goto(data, { waitUntil: 'load', timeout: 0 });
+    else await page.setContent(htmlTemplate(data));
+    const buffer = await page.screenshot();
+    await page.close();
+    terminateBrowser();
+    return buffer;
+}
+
+const htmlTemplate = (body) => {
+    return `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, 
+initial-scale=1.0"></head><body>${body}</body></html>`;
+}
+
+const isValidURL = (string) => {
+    try {
+        const parsedURL = new URL(string);
+        return (parsedURL.protocol && parsedURL.host) ? true : false;
+    } catch (error) {
+        return false;
+    }
+};
+
+//default route
+app.get('/ping', (req, res) => {
+    res.status(200).end('Application Started Pong!');
+});
+
+
+app.post('/process-thumbnail', async (req, res) => {
+    const { data, handleAsync = true } = req.body;
+    let resp = { success: true, message: 'initiated', error: null};
+        return res.status(400).send({ ...resp, message: "Cannot be processed", error: "Received invalid body", success: false });
+    if (handleAsync) res.send(resp);
+    
+    try {
+        let thumbnailBuffer;
+
+        thumbnailBuffer = await captureScreenshot(data);
+
+        resp = { ...resp, message: `Image process complete for object ${s3Path}`, result: [{ body: uploadStatus, metaData }] };
+        logger.info(resp.message);
+    } catch (error) {
+        logger.error('error =>' + error.message);
+        resp = { ...resp, message: `Image process failed for object ${s3Path}`, error: error.message, success: false };
+    } finally {
+        // console.log("resp thumbnail",resp);
+        if (!handleAsync) res.send(resp);
+        return resp
+    }
+});
+
+
+
+const port = 9500;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+module.exports = app;
